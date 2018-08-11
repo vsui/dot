3434307ba0cbc04ae4a6e602fd95bdd5c104eb0f
Victor <vsui2@illinois.edu>
2018-08-11 18:32:46 -0500

    Rename .vimrc to vimrc
    I don't want it to be a hidden file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
-ln -s $(pwd)/.vimrc ~/.vimrc
-ln -s $(pwd)/.vimrc ~/.config/neovim/init.vim
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/vimrc ~/.config/neovim/init.vim
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
