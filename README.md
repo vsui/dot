d39b11dcf38c586d88602ce60330b90d1ed75da1
Victor Sui <victorsui11@gmail.com>
2019-02-18 16:20:45 -0800

    Add README

---
+++ README.md
@@ -0,0 +1,1 @@
+As of now `bootstrap.sh` has to be executed from the project root.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 ln -s $(pwd)/vimrc ~/.vimrc
-if [ -d ~/.config/neovim ]; then
-	ln -s $(pwd)/vimrc ~/.config/neovim/init.vim
-fi
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
 
