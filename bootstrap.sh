52272ee2dff7d2c401de5ffc250b8b476a6ab63a
Victor Sui <victorsui11@gmail.com>
2020-06-01 20:31:19 +0000

    Add TODOs, exit on error, check for $XDG_CONFIG

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# TODO install nvim from source..
+# TODO CoC needs node installed...
+# TODO make script agnostic of folder
+# TODO auto install updated version of nvim / vim (nvim 4.3)
+# TODO maybe load plugs as on-demand instead of all at once
+# TODO maybe provide Makefile with `install` and `clean` targets
+
+set -e
+
+if [ -z $XDG_CONFIG_HOME ]
+then
+  echo 'Error: need to set $XDG_CONFIG_HOME'
+  exit 1
+fi
+
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/tmux.conf ~/.tmux.conf
 ln -s $(pwd)/emacs.d ~/.emacs.d
