473e53fbc36b52e8797a179dfc0c9d8e4f23d4bd
Victor Sui <victorsui11@gmail.com>
2020-06-01 20:34:14 -0700

    Changes for sudo, add -f flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,13 +10,22 @@
 # TODO maybe provide Makefile with `install` and `clean` targets
 # TODO cmake would be useful
 # TODO macOS?
+# TODO maybe put everything that doesn't need sudo together
 
 set -e
+set -v
+
+# this is set to -f if -f is passed to the script
+FORCE=""
+# TODO need to document this and maybe make it easier
+if [[ $* == *-f* ]]; then
+  FORCE="-f"
+fi
 
 if [ -z $XDG_CONFIG_HOME ]
 then
   # read -p '$XDG_CONFIG_HOME is not set. Would you like to continue by proceeding with the default XDG_CONFIG_HOME=~/.config? ' yn
-  echo '$XDG_CONFIG_HOME is not set. Would you like to proceed with the default XDG_CONFIG=~/.config?'
+  echo '$XDG_CONFIG_HOME is not set. Would you like to proceed with the default XDG_CONFIG_HOME=~/.config?'
   select yn in "Yes" "No"; do
     case $yn in
       Yes ) break;;
@@ -24,7 +33,11 @@ then
     esac
   done  
 
-  export XDG_CONFIG_HOME=~/.config
+  # This script is designed to be run with sudo but I need to expand ~ as the original user. This was the
+  # first way I could figure out to do this. There is probably a better solution (maybe running separating
+  # superuser commands from non-superuser commands into two separate scripts)
+  export XDG_CONFIG_HOME=`sudo -u $SUDO_USER /bin/bash -c "echo ~/.config"`
+  echo $XDG_CONFIG_HOME
 fi
 
 apt update
@@ -32,20 +45,28 @@ apt update
 apt install -y wget
 apt install -y curl
 apt install -y git
+apt install -y nodejs # need this for CoC
 
 # install neovim
 # TODO currently neovim files extracted to squashfs-root, maybe change to install them in /usr
 # TODO if neovim already installed exit
-wget -O nvim.appimage https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
-chmod u+x nvim.appimage && ./nvim.appimage --appimage-extract
-mkdir -p /usr/local/bin
-ln -s $(pwd)/squashfs-root/usr/bin/nvim /usr/local/bin/nvim
-apt install -y nodejs # need this for CoC
+NVIM_INSTALL_PATH=/usr/local/bin
+if test -e $NVIM_INSTALL_PATH && test -z $FORCE; then
+  echo "Error: file already exists at $NVIM_INSTALL_PATH"
+  exit 1
+fi
 
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
-ln -s $(pwd)/emacs.d ~/.emacs.d
+sudo -u $SUDO_USER wget -O nvim.appimage https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
+sudo -u $SUDO_USER chmod a+x nvim.appimage
+sudo -u $SUDO_USER ./nvim.appimage --appimage-extract
+mkdir -p /usr/local/bin # organize this line to somewhere else, doesn't fit with nvim code
+sudo -u $SUDO_USER mkdir -p $XDG_CONFIG_HOME/nvim
+ln -s $FORCE $(pwd)/vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -s $FORCE $(pwd)/squashfs-root/usr/bin/nvim $NVIM_INSTALL_PATH # TODO is it safe to hardcode `squashfs-root`
+rm nvim.appimage
 
-mkdir -p $XDG_CONFIG_HOME/nvim
-ln -s $(pwd)/vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -s $FORCE $(pwd)/vimrc ~/.vimrc
+ln -s $FORCE $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $FORCE $(pwd)/emacs.d ~/.emacs.d
+
 
