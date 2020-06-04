831d0e6e62bdf436d8ffcc90851ada110013e302
Victor Sui <victorsui11@gmail.com>
2020-06-04 05:48:23 +0000

    clang/ccls WIP

--- install.sh
+++ install.sh
@@ -58,6 +58,35 @@ fi
 # TODO also it seems to cause issues if ccls looks for the standard headers depending on the version of clang it was built with
 # apt install ccls # needed for CoC C++ support
 
+# install llvm+clang for ccls
+# TODO check signature
+# TODO set as default compiler
+
+# It seems like you can set this as the default with `update-alternatives`. Currently I am running into a
+# where it seems like clang++ is trying to use the standard library from c++ but it isn't installed correctly
+# or something. Maybe that was my original problem with the packaged version of `ccls`? I though that it wasn't
+# working since there was no compatible version of clang++ on my machine, but maybe not...
+# I tried to use libc++ instead of libstdc++ (clang implementation of std lib) but I am still running into issues.
+# I am possibly not installing the pre-compiled clang files properly (I just did a recursive copy to /usr/local).
+# Seems to have trouble finding the shared libraries when runing the binary.
+# Seems like you mainly need to set LD_LIBRARY_PATH
+#
+# TODO fix g++ installation or figure out how to use libstdc++ by default
+#
+
+LLVM_CLANG_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+LLVM_CLANG_PATH=$(pwd)/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04 # TODO extract this from URL
+wget $LLVM_CLANG_URL
+
+git clone --depth=1 --recursive https://github.com/MaskRay/ccls.git # TODO pin to release?
+pushd ccls
+
+cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=$LLVM_CLANG_PATH  # TODO add build parallelism?
+cmake --build Release
+ln -s $FORCE $(pwd)/Release/ccls /usr/local/bin/ccls
+ 
+popd
+
 sudo -u $SUDO_USER wget -O nvim.appimage https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
 sudo -u $SUDO_USER chmod a+x nvim.appimage
 sudo -u $SUDO_USER ./nvim.appimage --appimage-extract
