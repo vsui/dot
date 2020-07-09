283e9592a893929e8b535bc233656783d1dd9fe4
Victor Sui <victorsui11@gmail.com>
2020-07-08 21:10:15 -0500

    Fixup

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "Linux"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "macOS"
-else
-  echo "Error: $OSTYPE not supported"
+ else
+  echo "Error: $OSTYPE not supported" && false
 fi
 
 # this is set to -f if -f is passed to the script
@@ -26,18 +26,22 @@ if [[ $* == *-f* ]]; then
 fi
 
 function package_install {
-  if [[ "OSTYPE" == "linux-gnu"* ]]; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     apt install -y $@
-  elif [[ "OSTYPE" == "darwin"* ]]; then
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew install -y $@
+  else
+    echo "Internal error" && false
   fi
 }
 
 function package_update {
-  if [[ "OSTYPE" == "linux-gnu"* ]]; then
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     apt update
-  elif [[ "OSTYPE" == "darwin"* ]]; then
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
     brew update
+  else
+    echo "Internal error" && false
   fi
 }
 
@@ -67,31 +71,39 @@ package_install -y curl
 package_install -y git
 # install nodejs for CoC
 # TODO these are different versions of node
-if [[ "OSTYPE" == "linux-gnu"* ]]; then
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
   apt install -y nodejs
-elif [[ "OSTYPE" == "darwin"* ]]; then
+elif [[ "$OSTYPE" == "darwin"* ]]; then
   package-install -y node
+else
+  echo "Internal error" && false
 fi
-
 # install neovim
+package_install -y neovim
+
 # TODO currently neovim files extracted to squashfs-root, maybe change to install them in /usr
 # TODO if neovim already installed exit
-NVIM_INSTALL_PATH=/usr/local/bin
-if test -e $NVIM_INSTALL_PATH && test -z $FORCE; then
-  echo "Error: file already exists at $NVIM_INSTALL_PATH"
-  exit 1
-fi
+# NVIM_INSTALL_PATH=/usr/local/bin
+# if test -e $NVIM_INSTALL_PATH && test -z $FORCE; then
+#   echo "Error: file already exists at $NVIM_INSTALL_PATH"
+#   exit 1
+# fi
 
-sudo -u $SUDO_USER wget -O nvim.appimage https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
-sudo -u $SUDO_USER chmod a+x nvim.appimage
-sudo -u $SUDO_USER ./nvim.appimage --appimage-extract
-mkdir -p /usr/local/bin # organize this line to somewhere else, doesn't fit with nvim code
-sudo -u $SUDO_USER mkdir -p $XDG_CONFIG_HOME/nvim
-ln -s $FORCE $(pwd)/vimrc $XDG_CONFIG_HOME/nvim/init.vim
-ln -s $FORCE $(pwd)/squashfs-root/usr/bin/nvim $NVIM_INSTALL_PATH # TODO is it safe to hardcode `squashfs-root`
+# This isn't necessary anymore because it looks like neovim is in the package for ubuntu 20.04
+# TODO detect if neovim is right version or something... in apt
+# sudo -u $SUDO_USER wget -O nvim.appimage https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
+# sudo -u $SUDO_USER chmod a+x nvim.appimage
+# sudo -u $SUDO_USER ./nvim.appimage --appimage-extract
+# mkdir -p /usr/local/bin # organize this line to somewhere else, doesn't fit with nvim code
+# sudo -u $SUDO_USER mkdir -p $XDG_CONFIG_HOME/nvim
+# rm nvim.appimage
+
+mkdir -p $XDG_CONFIG_HOME/nvim
+# link vimrcs
 ln -s $FORCE $(pwd)/coc-settings.json $XDG_CONFIG_HOME/nvim/coc-settings.json
-rm nvim.appimage
+ln -s $FORCE $(pwd)/vimrc $XDG_CONFIG_HOME/nvim/init.vim
+# ln -s $FORCE $(pwd)/squashfs-root/usr/bin/nvim $NVIM_INSTALL_PATH # TODO is it safe to hardcode `squashfs-root`
 
 git config --global core.editor "nvim"
 export EDITOR=nvim # TODO need to actually add this to bash_profile or whatever
