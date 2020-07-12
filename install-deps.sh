7e6f4de37dc13574a5fdc631e642fe59585720dd
Victor Sui <victorsui11@gmail.com>
2020-07-11 23:46:21 -0500

    remove comment that causes bug

--- install-deps.sh
+++ install-deps.sh
@@ -7,9 +7,10 @@ apt-get update
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 
+# nodejs is needed for CoC.nvim
 apt-get install -y git \
                    cmake \
-                   nodejs \ # needed for CoC.vim
+                   nodejs \
                    neovim \
                    ccls \
                    i3
