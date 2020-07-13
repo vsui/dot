696e0a79a5186805aed3f1820d252df59ac07745
Victor Sui <victorsui11@gmail.com>
2020-07-12 23:04:31 -0500

    Add magit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -147,7 +147,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-journal use-package org-roam evil))))
+ '(package-selected-packages (quote (magit org-journal use-package org-roam evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
